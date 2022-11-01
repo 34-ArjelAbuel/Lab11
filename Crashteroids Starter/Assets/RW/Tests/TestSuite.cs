f8c9b02a9909c3f3c8f2c4e74834be08eb528299
dabeastt <arjeljabuel@gmail.com>
2022-11-01 09:54:09 +0800

    added setup and teardown

--- Crashteroids Starter/Assets/RW/Tests/TestSuite.cs
+++ Crashteroids Starter/Assets/RW/Tests/TestSuite.cs
@@ -11,15 +11,24 @@ namespace Tests
     {
         private Game game;
 
+        [SetUp]
+        public void Setup()
+        {
+            GameObject gameGameObject =
+                MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
+            game = gameGameObject.GetComponent<Game>();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            Object.Destroy(game.gameObject);
+        }
+
         // 1
         [UnityTest]
         public IEnumerator AsteroidsMoveDown()
         {
-            // 2
-            GameObject gameGameObject =
-                MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
-            game = gameGameObject.GetComponent<Game>();
-            // 3
             GameObject asteroid = game.GetSpawner().SpawnAsteroid();
             // 4
             float initialYPos = asteroid.transform.position.y;
@@ -27,16 +36,11 @@ namespace Tests
             yield return new WaitForSeconds(0.1f);
             // 6
             Assert.Less(asteroid.transform.position.y, initialYPos);
-            // 7
-            Object.Destroy(game.gameObject);
         }
 
         [UnityTest]
         public IEnumerator GameOverOccursOnAsteroidCollision()
         {
-            GameObject gameGameObject =
-               MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
-            Game game = gameGameObject.GetComponent<Game>();
             GameObject asteroid = game.GetSpawner().SpawnAsteroid();
             //1
             asteroid.transform.position = game.GetShip().transform.position;
@@ -45,8 +49,6 @@ namespace Tests
 
             //3
             Assert.True(game.isGameOver);
-
-            Object.Destroy(game.gameObject);
         }
 
     }
