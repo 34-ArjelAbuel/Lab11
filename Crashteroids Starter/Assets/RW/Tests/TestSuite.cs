205f815e7838544f423ba9ae22edca8a70ca9e8f
dabeastt <arjeljabuel@gmail.com>
2022-11-01 09:43:42 +0800

    passed asteroidsmovedown test

--- Crashteroids Starter/Assets/RW/Tests/TestSuite.cs
+++ Crashteroids Starter/Assets/RW/Tests/TestSuite.cs
@@ -4,25 +4,32 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
+
 namespace Tests
 {
     public class TestSuite
     {
-        // A Test behaves as an ordinary method
-        [Test]
-        public void TestSuiteSimplePasses()
+        private Game game;
+
+        // 1
+        [UnityTest]
+        public IEnumerator AsteroidsMoveDown()
         {
-            // Use the Assert class to test conditions
+            // 2
+            GameObject gameGameObject =
+                MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
+            game = gameGameObject.GetComponent<Game>();
+            // 3
+            GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+            // 4
+            float initialYPos = asteroid.transform.position.y;
+            // 5
+            yield return new WaitForSeconds(0.1f);
+            // 6
+            Assert.Less(asteroid.transform.position.y, initialYPos);
+            // 7
+            Object.Destroy(game.gameObject);
         }
 
-        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-        // `yield return null;` to skip a frame.
-        [UnityTest]
-        public IEnumerator TestSuiteWithEnumeratorPasses()
-        {
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
-            yield return null;
-        }
     }
 }
